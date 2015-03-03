4b029d0fec7da2578a19dfbe14c9de0c8dcf814a
davidebbo <david.ebbo@microsoft.com>
2015-03-02 20:43:26 -0800

    Add nuget restore to build file

--- build.cmd
+++ build.cmd
@@ -1,1 +1,2 @@
+nuget restore
 "%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" Slingshot.api\Slingshot.Api.csproj /p:webpublishmethod=filesystem;PublishUrl=artifacts;Configuration=Release /t:WebFileSystemPublish
